2867e74ff6d85a4dc3cb6afc318a4d5ff8929f1a
vikram <vikramramji24@gmail.com>
2026-02-10 15:33:14 +0530

    feat: Handle basic form submission and value states

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import FormInput from "./FormInput";
 
 const Form = () => {
+  const [email, setEmail] = useState("");
+  const [amount, setAmount] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${email}, Amount: ${amount}`);
+  };
+
   return (
-    <form action="" className="flex flex-col gap-6">
-      <FormInput label="Email" type="email" id="email" placeholder="name@example.com" />
-      <FormInput label="Amount" type="number" id="amount" placeholder="100.00" />
-      <button type="submit" className="bg-gray-900 text-white font-bold text-lg py-3 rounded-lg mt-4 cursor-pointer hover:bg-gray-700 active:scale-95 transition-all duration-300">
+    <form className="flex flex-col gap-6" onSubmit={handleSubmit}>
+      <FormInput
+        label="Email"
+        type="email"
+        id="email"
+        placeholder="name@example.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <FormInput
+        label="Amount"
+        type="number"
+        id="amount"
+        placeholder="100.00"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="bg-gray-900 text-white font-bold text-lg py-3 rounded-lg mt-4 cursor-pointer hover:bg-gray-700 active:scale-95 transition-all duration-300"
+      >
         Submit
       </button>
     </form>
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -5,17 +5,24 @@ const FormInput = ({
   required = true,
   autoComplete = "true",
   placeholder,
+  value,
+  onChange,
 }) => {
   return (
     <div className="flex flex-col gap-2">
-      <label htmlFor={id} className="font-semibold">{label}</label>
+      <label htmlFor={id} className="font-semibold">
+        {label}
+      </label>
       <input
         required={required}
         autoComplete={autoComplete}
         type={type}
         id={id}
         placeholder={placeholder || `Enter your ${label.toLowerCase()}`}
+        value={value}
+        onChange={onChange}
         className="text-lg border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:border-transparent"
+        {...(type === "number" && { min: "0.01", step: "0.01" })}
       />
     </div>
   );
